ccd58711ca91b53e8a3499146c1eef771d9fc890
Peter Tuthill <squareproton@gmail.com>
2018-10-13 20:58:11 +0100

    updated version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='PrintNodeApi',
-    version='0.1.1',
+    version='0.2.0',
     description="Python Client for PrintNode's API",
     author='printnode',
     author_email='support@printnode.com',
